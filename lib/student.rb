97b36b68caa2bde2ea56499c35b4964f6f01b93e
Gilbert Torchon <gilbert@noukod.com>
2018-11-26 21:18:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,7 @@ class Student
   def save
     sql="INSERT INTO students(name,grade) VALUES(?,?)"
     DB[:conn].execute(sql,self.name,self.grade)
+    self.id=DB[:conn].execute("SELECT last_insert_row_id()")[0][0]
   end
 
 end
